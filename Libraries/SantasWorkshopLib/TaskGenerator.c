548bc3452808664c5e607bcf1f4d62aa18370045
Dogukan Arat <aratdogukan@gmail.com>
2022-12-15 21:14:53 +0300

    Refactor tasks for part 2

--- Libraries/SantasWorkshopLib/TaskGenerator.c
+++ Libraries/SantasWorkshopLib/TaskGenerator.c
@@ -2,10 +2,11 @@
 #include <math.h>
 #include <stdlib.h>
 
-#define LUT_SIZE 18
+#define LUT_SIZE 20
 
 int lutTaskIdProb[LUT_SIZE] = 
 {
+    // %40 probability of TASK_TYPE_1
     TASK_TYPE_1,
     TASK_TYPE_1,
     TASK_TYPE_1,
@@ -15,18 +16,26 @@ int lutTaskIdProb[LUT_SIZE] =
     TASK_TYPE_1,
     TASK_TYPE_1,
 
+    // %10 more probability of TASK_TYPE_2 for part 2
+    TASK_TYPE_1,
+    TASK_TYPE_1,
+
+    // %20 probability of TASK_TYPE_2
     TASK_TYPE_2,
     TASK_TYPE_2,
     TASK_TYPE_2,
     TASK_TYPE_2,
 
+    // %20 probability of TASK_TYPE_3
     TASK_TYPE_3,
     TASK_TYPE_3,
     TASK_TYPE_3,
     TASK_TYPE_3,
 
+    // %5 probability of TASK_TYPE_4
     TASK_TYPE_4,
 
+    // %5 probability of TASK_TYPE_5
     TASK_TYPE_5,
 };
 
